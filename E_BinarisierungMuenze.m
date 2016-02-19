82bba8c8948240ad3c5b6f12d84d9f6c788ece61
Domi <test@gmail.com>
2016-02-19 17:19:28 +0100

    alle m dateien gelöscht

--- E_BinarisierungMuenze.m
+++
@@ -1,8 +0,0 @@
-%Das eingelesene Bild wird Binarisiert. Der Grenzwert wird mit übergeben.
-%Das Binarisierte Bild wird dann gleich weiterverwendet um die Anzahl der
-%Ringe zu erkennen.
-function kant_Binar = E_BinarisierungMuenze(Kantenbild,Grenzwert)
-double ebene;
-[z,s,t] = size(Kantenbild);
-kant_Binar = zeros(z,s,t,'uint8');
-
